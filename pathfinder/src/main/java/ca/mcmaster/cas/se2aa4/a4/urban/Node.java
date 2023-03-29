323660b2ed28acb30a897ece284731c40bd994ae
Nirmal-code <nirmal.chaudhari2003@gmail.com>
2023-03-29 13:35:42 -0400

    Implemented F88 and F89: Nodes and Edges
    Graph ADT has defined nodes and edges.

---
+++ pathfinder/src/main/java/ca/mcmaster/cas/se2aa4/a4/urban/Node.java
@@ -0,0 +1,17 @@
+package ca.mcmaster.cas.se2aa4.a4.urban;
+
+public class Node {
+
+    private Double x;
+    private Double y;
+    private String city_name;
+
+
+    public Node(Double x, Double y, String name){
+        this.x=x;
+        this.y=y;
+        this.city_name=name;
+    }
+
+
+}
