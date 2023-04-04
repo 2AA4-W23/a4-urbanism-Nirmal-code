dcda03c7cda461941aca490d6e7a9f4793f8be1b
Nirmal-code <nirmal.chaudhari2003@gmail.com>
2023-04-03 21:01:22 -0400

    Forgot to add capitol as VertexElement
    Capitol is now also a VertexElement.

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/TilesTypes/VertexElement.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/TilesTypes/VertexElement.java
@@ -1,7 +1,7 @@
 package ca.mcmaster.cas.se2aa4.a3.island.TilesTypes;
 
 public enum VertexElement {
-    WATER,LAND,SMALL_CITY,BIG_CITY,ROAD;
+    WATER,LAND,SMALL_CITY,BIG_CITY,CAPITOL,ROAD;
 
 
 }
